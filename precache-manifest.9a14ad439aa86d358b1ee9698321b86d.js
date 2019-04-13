378a46aa23f8cdf7d9e0572c562e9948405a7e9e
abhi-debuginside <abhijth@hotmail.com>
2019-04-13 16:18:50 +0530

    Updates

---
+++ precache-manifest.9a14ad439aa86d358b1ee9698321b86d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "96fa0ed244f1a1cf68ba",
+    "url": "/blog/static/css/main.95d63eeb.chunk.css"
+  },
+  {
+    "revision": "96fa0ed244f1a1cf68ba",
+    "url": "/blog/static/js/main.12e1576c.chunk.js"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/blog/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7992d72ff6f886b8d8640194db9b9816",
+    "url": "/blog/index.html"
+  }
+];
