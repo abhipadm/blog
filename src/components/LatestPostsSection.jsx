e0f02748b5a0c1e5f3642c1ab2b92f5113eae127
Abhijith <abhijth@hotmail.com>
2020-08-04 00:52:29 +0530

    latest post layout

--- src/components/LatestPostsSection.jsx
+++ src/components/LatestPostsSection.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { PropTypes } from "prop-types";
-import { Container } from "@material-ui/core";
 import {
   Grid,
-  GridListTile,
-  ListSubheader,
   Typography,
   Card,
   CardActionArea,
@@ -12,6 +9,7 @@ import {
   CardContent,
   CardActions,
   Button,
+  Grow,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -22,20 +20,20 @@ const useStyles = makeStyles((theme) => ({
   },
   postCard: {
     minHeight: 330,
-    padding: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
   },
   cardMedia: {
-    height: 140,
+    height: 350,
   },
   cardActionArea: {
     minHeight: 300,
   },
   cardContent: {
-    paddingTop: theme.spacing(3),
+    paddingTop: theme.spacing(1),
   },
   shortDescription: {
-    overflow: "Hidden",
     whiteSpace: "break-spaces",
+    color: theme.palette.secondary.contrastText,
   },
 }));
 
@@ -53,41 +51,43 @@ const LatestPostsSection = (props) => {
 
       {latestPosts.map((post) => (
         <Grid item xs={12} md={4} key={post.id}>
-          <Card className={classes.postCard}>
-            <CardActionArea className={classes.CardActionArea}>
-              <CardMedia
-                component="img"
-                alt={post.title}
-                image={post.img}
-                title={post.title}
-                className={classes.cardMedia}
-              />
-              <CardContent className={classes.cardContent}>
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  component="h2"
-                  color="primary"
-                >
-                  {post.title}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  className={classes.shortDescription}
-                  color="textSecondary"
-                  component="p"
-                >
-                  {post.shortDescription}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
+          <Grow in true timeout={1000}>
+            <Card className={classes.postCard}>
+              <CardActionArea className={classes.CardActionArea}>
+                <CardMedia
+                  component="img"
+                  alt={post.title}
+                  image={post.img}
+                  title={post.title}
+                  className={classes.cardMedia}
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="h2"
+                    color="primary"
+                  >
+                    {post.title}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    className={classes.shortDescription}
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {post.shortDescription}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
 
-            <CardActions disableSpacing>
-              <Button size="small" color="primary">
-                Read More
-              </Button>
-            </CardActions>
-          </Card>
+              <CardActions disableSpacing>
+                <Button size="small" color="primary">
+                  Read More
+                </Button>
+              </CardActions>
+            </Card>
+          </Grow>
         </Grid>
       ))}
     </Grid>
